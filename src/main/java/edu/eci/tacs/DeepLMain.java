73b9aa2b378da09afa9c1a325f5c16e65f966cd1
Camu10 <manuel.murillo.1999@gmail.com>
2020-09-26 10:52:07 -0500

    Refactoring DeepL test

--- src/main/java/edu/eci/tacs/DeepLMain.java
+++ src/main/java/edu/eci/tacs/DeepLMain.java
@@ -6,26 +6,58 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DeepLMain {
-
+    /**
+     * Ingreso a la aplicación
+     *
+     * @param args Los argumentos de entrada.
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.deepl.com/translator");
-        traductor(driver);
+        traductorIngles(driver);
+        traductorFrances(driver);
+        traductorAleman(driver);
         driver.close();
     }
 
-    public static void traductor(WebDriver driver) throws InterruptedException {
+    /**
+     * Test para traducir un texto en ingles al español.
+     * @param driver El Selenium Web Driver para google chrome.
+     * @throws InterruptedException
+     */
+    public static void traductorIngles(WebDriver driver) throws InterruptedException {
         WebElement input = driver.findElement(By.className("lmt__textarea_container"));
         input.click();
         input = driver.findElement(By.xpath("//*[@class=\"lmt__inner_textarea_container\"]/textarea"));
         input.sendKeys("This text is to test the deepl application Web whit selenium WebDriver");
         Thread.sleep(5000);
         input.clear();
+    }
+
+    /**
+     * Test para traducir un texto en frances al español.
+     * @param driver El Selenium Web Driver para google chrome.
+     * @throws InterruptedException
+     */
+    public static void traductorFrances(WebDriver driver) throws InterruptedException {
+        WebElement input = driver.findElement(By.className("lmt__textarea_container"));
+        input.click();
         input = driver.findElement(By.xpath("//*[@class=\"lmt__inner_textarea_container\"]/textarea"));
         input.sendKeys("Ce texte est destiné à tester l'application deepl Web whit selenium WebDriver");
         Thread.sleep(5000);
         input.clear();
+    }
+
+    /**
+     * Test para traducir un texto en ALeman al español.
+     * @param driver El Selenium Web Driver para google chrome.
+     * @throws InterruptedException
+     */
+    public static void traductorAleman(WebDriver driver) throws InterruptedException {
+        WebElement input = driver.findElement(By.className("lmt__textarea_container"));
+        input.click();
         input = driver.findElement(By.xpath("//*[@class=\"lmt__inner_textarea_container\"]/textarea"));
         input.sendKeys("Dieser Text soll die Tiefenanwendung Web mit Selenium WebDriver testen.");
         Thread.sleep(5000);
